5c0a8d383da60d86a9c7376b889a292e95c008dd
Lukas Andriuška <60364075+TheLuk1s@users.noreply.github.com>
2024-04-08 21:48:34 +0300

    Routing fixes

--- routes/api.php
+++ routes/api.php
@@ -14,22 +14,21 @@ Route::controller(RestaurantController::class)->group(function () {
     Route::get('restaurants', 'index');
     Route::get('restaurants/{restaurant}', 'show');
     Route::post('restaurants', 'store');
-    Route::put('restaurants/{restaurant}', 'store');
+    Route::put('restaurants/{restaurant}', 'update');
     Route::delete('restaurants/{restaurant}', 'destroy');
 });
 
 Route::controller(RestaurantTableController::class)->group(function () {
     Route::get('restaurants/{restaurant}/tables', 'index');
-    Route::post('restaurants/{restaurant}/tables', 'store');
     Route::get('restaurants/{restaurant}/tables/{table}', 'show');
+    Route::post('restaurants/{restaurant}/tables', 'store');
     Route::put('restaurants/{restaurant}/tables/{table}', 'update');
     Route::delete('restaurants/{restaurant}/tables/{table}', 'destroy');
 });
 
 Route::controller(RestaurantReservationController::class)->group(function () {
     Route::get('restaurants/{restaurant}/reservations', 'index');
+    Route::get('restaurants/{restaurant}/reservations/{reservation}', 'show');
     Route::post('restaurants/{restaurant}/reservations', 'store');
-    // Route::get('restaurants/{restaurant}/tables/{table}', 'show');
-    // Route::put('restaurants/{restaurant}/tables/{table}', 'update');
-    // Route::delete('restaurants/{restaurant}/tables/{table}', 'destroy');
+    Route::delete('restaurants/{restaurant}/reservations/{table}', 'destroy');
 });
